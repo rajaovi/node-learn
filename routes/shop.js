c3edba7affb97a8e8475d659bb8bc0a89f87ed63
Raja <orajacse@gmail.com>
2025-03-18 14:07:35 +0530

    Sharing data across Request and user

--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,13 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  console.log("adminData", adminData.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
