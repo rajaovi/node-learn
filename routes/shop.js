d3bbed1424df9fbb207fa88ee2adc09772b85d19
Raja <orajacse@gmail.com>
2025-03-19 14:15:25 +0530

    working dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const productControllers = require("../controllers/products");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", productControllers.getProducts);
+router.get("/", productsController.getProducts);
 
 module.exports = router;
